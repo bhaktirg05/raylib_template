d6b298c37e027076b7da9d31fe96de4ce366e1e3
Bhakti Gotipamul <bhaktigotipamul5@gmail.com>
2026-09-25 18:19:24 +0530

    Pushing template for 2nd time

--- geometry.js
+++ geometry.js
@@ -3,6 +3,16 @@ function calcOffset(outer, inner) {
     return (outer - inner) / 2;
 }
 
+function centerOfRectangleCoordinates(x, y) {
+    return x + y / 2;
+}
+
+function newCoordinates(x, y) {
+    return x - y / 2;
+}
+
 module.exports = {
     calcOffset,
+    centerOfRectangleCoordinates,
+    newCoordinates,
 };
